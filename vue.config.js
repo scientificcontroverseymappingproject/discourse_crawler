a785ba1a725bc8fcb6bf2b26463a71f84e3c734e
milesccoleman <milesccoleman@gmail.com>
2023-09-29 13:58:01 -0400

    trying to get get to work

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/discourse_crawler/" : "/",
-  devServer: {
-    allowedHosts: [".csb.app"], // <  Copy and paste the domain from the URL Codebox provides for your runtime here
-  },
 };
