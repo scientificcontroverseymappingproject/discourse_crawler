3a340175eae569300fd7a13bbcdedcc3fa6480ef
Miles Coleman <milescoleman@Miless-Air.fios-router.home>
2024-06-13 15:11:46 -0400

    fixed typo

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === "production" ? "/discourse_crawler/" : "/",
   devServer: {
     allowedHosts: [".csb.app"], // Copy and paste the domain from the URL Codebox provides for your runtime here
   },
